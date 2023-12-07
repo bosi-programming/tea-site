20f7a4c0b994d1dc7ce437d20441022e7cddfe6a
bosifullstack <bosiarquitetura@gmail.com>
2023-12-07 14:43:17 -0300

    feat: add 1/20 concentration

--- src/App.types.ts
+++ src/App.types.ts
@@ -1,2 +1,2 @@
 export type TStrength = 'weak' | 'weakest' | 'normal' | 'strong' | 'strongest';
-export type TConcentration = '1/10' | '1/15' | '1/30' | '1/50' | '1/100';
+export type TConcentration = '1/10' | '1/15' | '1/20' | '1/30' | '1/50' | '1/100';
