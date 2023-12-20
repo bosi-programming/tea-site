51f39929d513ff01257071a6f47ad56916c72529
bosifullstack <bosiarquitetura@gmail.com>
2023-12-20 11:34:28 -0300

    Revert "feat[infusions] add a function to calculate infusion time"
    This reverts commit 23cabbcdc8d8cec23e5669601831c72de8ef4b02.

--- src/modules/StartInfusionSection/StartInfusionSection.tsx
+++ src/modules/StartInfusionSection/StartInfusionSection.tsx
@@ -1,5 +1,5 @@
 import { useIntl } from 'react-intl';
-import { calculateInfusionTime } from '@/App.constants';
+import { BASE_INFUSION_TIME } from '@/App.constants';
 import { TConcentration, TStrength } from '@/App.types';
 import { Cup } from '@/icons/Cup';
 import { Leaf } from '@/icons/Leaf';
@@ -35,15 +35,21 @@ export function StartInfusionSection({ strength, concentration, size }: Infusion
           />{' '}
         </span>
         {
-          calculateInfusionTime(strength, concentration).length
+          BASE_INFUSION_TIME[strength][
+            concentration
+          ].length
         }{' '}
         {intl.formatMessage({ id: 'steeps' })}{' '}
-        {calculateInfusionTime(strength, concentration).join(', ')}{' '}
+        {BASE_INFUSION_TIME[strength][
+          concentration
+        ].join(', ')}{' '}
         {intl.formatMessage({ id: 'seconds' })}
       </Paragraph>
       <Timer
         infusionTime={
-          calculateInfusionTime(strength, concentration)
+          BASE_INFUSION_TIME[strength][
+          concentration
+          ]
         }
       />
     </>)
