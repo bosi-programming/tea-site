5c018e46867af0e4a7f876739a1b630c55323b9f
bosifullstack <bosiarquitetura@gmail.com>
2024-02-20 21:59:36 -0300

    Make tea-app a pwa

---
+++ public/serviceWorker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'tea-v1';
+const urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  console.log('fetch', event);
+  event.respondWith(caches.match(event.request).then(res => {
+    if (res) {
+      return res;
+    }
+    return fetch(event.request);
+  }))
+})
